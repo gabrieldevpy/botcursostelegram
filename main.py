1e0b33286be24527f462de5e656f2f923dea451e
gabrieldevpy <123579810+gabrieldevpy@users.noreply.github.com>
2025-02-08 08:37:38 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import os
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler
-from handlers import add_conv, edit_conv, del_conv, start, list_courses, get_course_link, list_courses_button
+from handlers import (
+    add_conv,
+    edit_conv,
+    del_conv,
+    start,
+    list_courses,
+    get_course_link,
+    list_courses_button,
+    cancel_operation
+)
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,7 +18,7 @@ def main():
     bot_token = os.getenv("BOT_TOKEN")
     if not bot_token:
         raise ValueError("Token do bot não configurado!")
-
+    
     app = Application.builder().token(bot_token).build()
     
     # Adiciona os CommandHandlers
@@ -17,8 +26,9 @@ def main():
     app.add_handler(CommandHandler("listar_cursos", list_courses))
     app.add_handler(CommandHandler("curso", get_course_link))
     
-    # Adiciona o CallbackQueryHandler para o botão "listar_cursos"
+    # Adiciona os CallbackQueryHandlers para os botões inline
     app.add_handler(CallbackQueryHandler(list_courses_button, pattern="^listar_cursos$"), group=-1)
+    app.add_handler(CallbackQueryHandler(cancel_operation, pattern="^cancelar_operacao$"), group=-1)
     
     # Adiciona os ConversationHandlers
     app.add_handler(add_conv)
